03a52785ba6c9fb015506e6949716fa2c5179500
DANKAMN <danielnicholas1477@gmail.com>
2024-03-04 09:10:27 +0100

    Updated the header and make the profile page private

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -23,7 +23,7 @@ const OAuth = () => {
             },
             body: JSON.stringify({ name: result.user.displayName, email: result.user.email, photo: result.user.photoURL })
         })
-        const data = res.json()
+        const data = await res.json()
         dispatch(signInSuccess(data))
          
         navigate('/')
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import OAuth from '../components/OAuth'
 
 const SignUp = () => {
   const [formData, setFormData] = useState({})
